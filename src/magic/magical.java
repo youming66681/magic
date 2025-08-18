4c3624c608aa8541c2bed398ef3f297ae9536a20
magical <3030854060@qq.com>
2025-08-18 19:56:35 +0800

    1

--- src/magic/magical.java
+++ src/magic/magical.java
@@ -7,7 +7,8 @@ import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
 public class magic extends Mod{
-
+    public static String ModName = "magic";
+    public SFireMod() {}
     @Override
     public void loadContent(){
         MLItems.load();
