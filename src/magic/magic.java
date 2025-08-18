2793c454da1c35a71aeec63dd9d568500de1cf0c
magical <3030854060@qq.com>
2025-08-18 18:12:21 +0800

    1

--- src/magic/magic.java
+++ src/magic/magic.java
@@ -1,18 +1,15 @@
-package example;
+package magic;
 
 import arc.*;
 import arc.util.*;
-import mindustry.*;
-import mindustry.content.*;
 import mindustry.game.EventType.*;
-import mindustry.gen.*;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class magic extends Mod{
 
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
+    public magic(){
+        Log.info("Loaded magic constructor.");
 
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
@@ -21,7 +18,7 @@ public class ExampleJavaMod extends Mod{
                 BaseDialog dialog = new BaseDialog("frog");
                 dialog.cont.add("behold").row();
                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+                dialog.cont.image(Core.atlas.find("magic")).pad(20f).row();
                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
                 dialog.show();
             });
@@ -30,7 +27,8 @@ public class ExampleJavaMod extends Mod{
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        Log.info("Loading some magic content.");
+        MLItems.load();
     }
 
 }
