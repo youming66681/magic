0edcc05ff01b3113fb73bd5a709eb913774329a7
magical <3030854060@qq.com>
2025-08-21 17:32:41 +0800

    1

---
+++ src/magical/content/MLItems.java
@@ -0,0 +1,13 @@
+package magical.content;
+
+public class MLItems {
+    public static void load(){
+        public static Item hg = new Item("幻钢", Color.valueOf("97B5EDFF")) {
+            {
+                description = "用于建造幻境工业的开端";
+                hardness = 3;
+                cost = 1.0F;
+            }
+        };
+    }
+}
