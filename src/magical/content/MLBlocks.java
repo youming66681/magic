34c94f9405e260317707393ca6acdf84f1783523
magical <3030854060@qq.com>
2026-04-16 03:36:11 +0800

    1

--- src/magical/content/MLBlocks.java
+++ src/magical/content/MLBlocks.java
@@ -4,25 +4,25 @@ import mindustry.content.Blocks;
 import magical.content.MLItems;
 
 public class MLBlocks {
-    public static 幻钢矿 = new OreBlock(Items.幻钢){{
+    public static oreHg = new OreBlock(Items.幻钢){{
         variants = 4;
         oreDefault = true;
         oreThreshold = 0.8f;
         oreScale = 21.0f;
     }};
-    public static 石墨矿 = new OreBlock(Items.graphite){{
+    public static oreSm = new OreBlock(Items.graphite){{
         variants = 3;
         oreDefault = true;
         oreThreshold = 0.8f;
         oreScale = 21.0f;
     }};
-    public static 翼石矿 = new OreBlock(Items.翼石){{
+    public static oreYs = new OreBlock(Items.翼石){{
         variants = 5;
         oreDefault = true;
         oreThreshold = 0.9f;
         oreScale = 23.0f;
     }};
-    public static 荧羽石矿 = new OreBlock(Items.荧羽石){{
+    public static oreYys = new OreBlock(Items.荧羽石){{
         variants = 6;
         oreDefault = true;
         oreThreshold = 0.8f;
