c8d10fae33c56d191a7d181e7c95c584052e2fa5
magical <3030854060@qq.com>
2026-04-16 02:13:50 +0800

    1

--- src/magical/content/MLBlocks.java
+++ src/magical/content/MLBlocks.java
@@ -1,49 +1,5 @@
 package magical.content;
 
-import arc.Core;
-import arc.graphics.Color;
-import arc.graphics.g2d.Draw;
-import arc.graphics.g2d.Fill;
-import arc.graphics.g2d.Lines;
-import arc.math.*;
-import arc.struct.Seq;
-import arc.util.Tmp;
-import mindustry.content.*;
-import mindustry.entities.Effect;
-import mindustry.entities.UnitSorts;
-import mindustry.entities.abilities.MoveEffectAbility;
-import mindustry.entities.abilities.StatusFieldAbility;
-import mindustry.entities.bullet.*;
-import mindustry.entities.effect.*;
-import mindustry.entities.part.HaloPart;
-import mindustry.entities.part.RegionPart;
-import mindustry.entities.part.ShapePart;
-import mindustry.entities.pattern.*;
-import mindustry.gen.*;
-import mindustry.graphics.CacheLayer;
-import mindustry.graphics.Drawf;
-import mindustry.graphics.Layer;
-import mindustry.graphics.Pal;
-import mindustry.type.*;
-import mindustry.type.unit.MissileUnitType;
-import mindustry.world.*;
-import mindustry.world.blocks.defense.*;
-import mindustry.world.blocks.defense.turrets.*;
-import mindustry.world.blocks.distribution.*;
-import mindustry.world.blocks.environment.*;
-import mindustry.world.blocks.liquid.ArmoredConduit;
-import mindustry.world.blocks.liquid.Conduit;
-import mindustry.world.blocks.liquid.LiquidBridge;
-import mindustry.world.blocks.liquid.LiquidRouter;
-import mindustry.world.blocks.payloads.*;
-import mindustry.world.blocks.power.*;
-import mindustry.world.blocks.production.*;
-import mindustry.world.blocks.storage.*;
-import mindustry.world.blocks.units.*;
-import mindustry.world.consumers.*;
-import mindustry.world.draw.*;
-import mindustry.world.meta.*;
-
 public class MLBlocks {
 
     public static void load(){}
