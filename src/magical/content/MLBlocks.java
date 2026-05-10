c183852aaba0566d022360b3256ebdc4399a02dd
magical <3030854060@qq.com>
2026-05-10 17:09:02 +0800

    1

--- src/magical/content/MLBlocks.java
+++ src/magical/content/MLBlocks.java
@@ -1,32 +1,18 @@
 package magical.content;
 
+import mindustry.content.Fx;
+import mindustry.content.Items;
+import mindustry.entities.Effect;
+import mindustry.entities.bullet.BasicBulletType;
+import mindustry.type.Category;
+import mindustry.type.ItemStack;
+import mindustry.world.Block;
+import mindustry.world.blocks.defense.turrets.ItemTurret;
+import mindustry.world.meta.BlockGroup;
 import mindustry.content.Blocks;
 import magical.content.MLItems;
 
 public class MLBlocks {
-    public static OreBlock 幻钢矿 = new OreBlock(Items.幻钢){{
-        variants = 4;
-        oreDefault = true;
-        oreThreshold = 0.8f;
-        oreScale = 21.0f;
-    }};
-    public static OreBlock 石墨矿 = new OreBlock(Items.graphite){{
-        variants = 3;
-        oreDefault = true;
-        oreThreshold = 0.8f;
-        oreScale = 21.0f;
-    }};
-    public static OreBlock 翼石矿 = new OreBlock(Items.翼石){{
-        variants = 5;
-        oreDefault = true;
-        oreThreshold = 0.9f;
-        oreScale = 23.0f;
-    }};
-    public static OreBlock 荧羽石矿 = new OreBlock(Items.荧羽石){{
-        variants = 6;
-        oreDefault = true;
-        oreThreshold = 0.8f;
-        oreScale = 18.0f;
-    }};
+
     public static void load(){}
 }
