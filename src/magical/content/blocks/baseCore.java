68d37249fc0c326757b7b4974d77e644d41464d3
magical <3030854060@qq.com>
2026-05-12 17:07:18 +0800

    1

--- src/magical/content/blocks/baseCore.java
+++ src/magical/content/blocks/baseCore.java
@@ -6,7 +6,7 @@ import mindustry.type.ItemStack;
 import mindustry.world.blocks.storage.CoreBlock;
 import mindustry.world.Tile;
 import mindustry.game.Team;
-
+import mindustry.Vars;
 
 public class baseCore extends CoreBlock{
 
@@ -16,15 +16,15 @@ public class baseCore extends CoreBlock{
 
     @Override
     public boolean canBreak(Tile tile){
-        return state.teams.cores(tile.team()).size > 1;
+        return Vars.state.teams.cores(tile.team()).size > 1;
     }
 
     @Override
     public boolean canPlaceOn(Tile tile, Team team, int rotation){
         if(tile == null) return false;
         //in the editor, you can place them anywhere for convenience
-        if(state.isEditor()) return true;
-        if(!state.isEditor()) return true;
+        if(Vars.state.isEditor()) return true;
+        if(!Vars.state.isEditor()) return true;
 
         CoreBuild core = team.core();
 
@@ -35,7 +35,7 @@ public class baseCore extends CoreBlock{
         }
 
         //must have all requirements
-        if(core == null || (!state.rules.infiniteResources && !core.items.has(requirements, state.rules.buildCostMultiplier))) return false;
+        if(core == null || (!Vars.state.rules.infiniteResources && !core.items.has(requirements, Vars.state.rules.buildCostMultiplier))) return false;
 
         return tile.block() instanceof CoreBlock && size > tile.block().size && (!requiresCoreZone || tempTiles.allMatch(o -> o.floor().allowCorePlacement));
     }
