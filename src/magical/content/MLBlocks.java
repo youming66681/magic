b8fbdb4895c162ec5c3cc975709d2cf8f3703564
magical <3030854060@qq.com>
2026-05-13 17:26:27 +0800

    1

--- src/magical/content/MLBlocks.java
+++ src/magical/content/MLBlocks.java
@@ -59,7 +59,7 @@ public class MLBlocks {
                     ItemStack.with(new Object[]{MLItems.phantomSteel, 5000, Items.titanium, 3000, Items.graphite, 1000})
             );
 
-            consumeItem(ItemStack.with(new Object[]{(MLItems.phantomSteel, 1, Items.titanium, 1}));
+            consumeItem(ItemStack.with(new Object[]{MLItems.phantomSteel, 1, Items.titanium, 1}));
             consumePower(1.0f);
         }};
     }
