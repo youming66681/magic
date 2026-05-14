bfeb01a77838be954120a3d884cea4cbdf767838
magical <3030854060@qq.com>
2026-05-14 19:53:25 +0800

    magic

--- src/magical/content/MLBlocks.java
+++ src/magical/content/MLBlocks.java
@@ -30,12 +30,6 @@ public class MLBlocks {
     public static Block xuanCrystalManufacturingMachine;
 
     public static void load() {
-        phantomSteelOre = new OreBlock(MLItems.phantomSteel){{
-            variants = 4;
-            oreDefault = true;
-            oreThreshold = 0.8f;
-            oreScale = 21.0f;
-        }};
         baseCore = new baseCore("baseCore") {{
             requirements(Category.effect, ItemStack.with(new Object[]{MLItems.phantomTitaniumSteel, 200, MLItems.mysticCrystal, 200, MLItems.phantomLuminousAlloy, 100}));
 
