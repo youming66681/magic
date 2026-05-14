a9cd2e7ed50b5b54a993d60c54037a84ea923e42
magical <3030854060@qq.com>
2026-05-14 16:35:37 +0800

    1

--- src/magical/content/MLTechTree.java
+++ src/magical/content/MLTechTree.java
@@ -2,7 +2,7 @@ package magical.content;
 
 import static mindustry.content.TechTree.*;
 import mindustry.type.ItemStack;
-import mindustry.content.Planets
+import mindustry.content.Planets;
 
 import magical.content.MLItems;
 import magical.content.MLBlocks;
