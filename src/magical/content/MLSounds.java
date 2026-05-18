22d55c7b77df7df969ed0af61e02d416d62cef22
magical <3030854060@qq.com>
2026-05-18 19:03:52 +0800

    magic

--- src/magical/content/MLSounds.java
+++ src/magical/content/MLSounds.java
@@ -8,9 +8,36 @@ public class MLSounds {
     public static Sound explosionAfflict;
     public static Sound explosionCleroi;
 
-    public static void load(){
-        Mod mod = Vars.mods.getMod(magical.magic.class);
-        explosionAfflict = mod.sound("explosionAfflict");
-        explosionCleroi = mod.sound("explosionCleroi");
+    public static void alertLoop() {
+        if (!Vars.headless) {
+            Vars.control.sound.loop(alert2, 2.0F);
+        }
+
+    }
+
+    public static void load() {
+        try {
+            for(Field field : MLSounds.class.getFields()) {
+                if (field.getType().equals(Sound.class)) {
+                    field.set((Object)null, loadSound(field.getName()));
+                }
+            }
+        } catch (IllegalAccessException e) {
+            Log.err(e);
+        }
+
+    }
+
+    private static Sound loadSound(String soundName) {
+        if (Vars.headless) {
+            return new Sound();
+        } else {
+            String path = "sounds/" + soundName;
+            String filePath = Vars.tree.get(path + ".ogg").exists() ? path + ".ogg" : path + ".mp3";
+            Sound sound = new Sound();
+            AssetDescriptor<?> desc = Core.assets.load(filePath, Sound.class, new SoundLoader.SoundParameter(sound));
+            desc.errored = Throwable::printStackTrace;
+            return sound;
+        }
     }
 }
