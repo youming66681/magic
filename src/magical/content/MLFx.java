27ed7e2be4a99be035051f81fe6e617e2d73d6ec
magical <3030854060@qq.com>
2026-05-18 18:47:45 +0800

    magic

--- src/magical/content/MLFx.java
+++ src/magical/content/MLFx.java
@@ -10,15 +10,16 @@ import mindustry.entities.Effect;
 
 public class MLFx {
     public static final Rand rand = new Rand();
-    public static final Vec2 v = new Vec2();
 
     public static Effect smallElectricDetonation = new Effect(30, e -> {
         Draw.color(Color.valueOf("#FEEBB3FF"), e.color, e.finpow());
+
+        Vec2 temp = new Vec2();
         for(int i = 0; i < 8; i++){
-        randLenVectors(e.id, i, 2f + e.fin() * 4f, (x, y) -> {
-            Fill.square(e.x + x, e.y + y, 0.5f + e.fout() * 2f, 45);
-             }
-        });
+            Mathf.randLenVectors(e.id, i, 2f + e.fin() * 4f, temp);
+            Fill.square(e.x + temp.x, e.y + temp.y, 0.5f + e.fout() * 2f, 45);
+        }
     });
+
     public static void load(){}
 }
