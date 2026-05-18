c277f451a92d468ad3015732bd18f4d02cdbc520
magical <3030854060@qq.com>
2026-05-18 18:38:17 +0800

    magic

--- src/magical/content/MLFx.java
+++ src/magical/content/MLFx.java
@@ -13,9 +13,9 @@ public class MLFx {
     public static final Vec2 v = new Vec2();
 
     public static Effect smallElectricDetonation = new Effect(30, e -> {
+        Draw.color(Color.valueOf("#FEEBB3FF"), e.color, e.finpow());
         Mathf.randLenVectors(e.id, 8, 2f + e.fin() * 4f, (x, y) -> {
             Fill.square(e.x + x, e.y + y, 0.5f + e.fout() * 2f, 45);
-            Draw.color(Color.valueOf("#FEEBB3FF"), e.color, e.finpow());
         });
     });
     public static void load(){}
