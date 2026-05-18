648bea5fb5c2f1420cb83b6b2059d19acbaace4d
magical <3030854060@qq.com>
2026-05-18 19:00:01 +0800

    magic

--- src/magical/content/MLSounds.java
+++ src/magical/content/MLSounds.java
@@ -2,13 +2,14 @@ package magical.content;
 
 import arc.audio.Sound;
 import mindustry.Vars;
+import mindustry.mod.Mod;
 
 public class MLSounds {
     public static Sound explosionAfflict;
     public static Sound explosionCleroi;
 
     public static void load(){
-        Mod mod = Vars.mods.getMod(magical.Magic.class);
+        Mod mod = Vars.mods.getMod(magical.magic.class);
         explosionAfflict = mod.sound("explosionAfflict");
         explosionCleroi = mod.sound("explosionCleroi");
     }
