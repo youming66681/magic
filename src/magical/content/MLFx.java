7ffbdff73090a13ede6a93db4b29441d25b5cbc5
magical <3030854060@qq.com>
2026-05-20 18:18:18 +0800

    magic

--- src/magical/content/MLFx.java
+++ src/magical/content/MLFx.java
@@ -16,23 +16,25 @@ import mindustry.type.*;
 import mindustry.content.*;
 
 public class MLFx {
+    public static Effect smallElectricDetonation;
+
     public static final Rand rand = new Rand();
     Vec2 temp = new Vec2();
 
-    public static Effect smallElectricDetonation = new Effect(30f, (e) -> {
-        Draw.color(Color.valueOf("97B5EDFF"), e.color, e.fin() + 0.4F);
-        e.scaled(6, (i) -> {
-            Lines.stroke(3f * i.foutpow());
-            Lines.circle(i.x, i.y, i.fin(Interp.circleOut) * 15f * 15F);
+    public static void load() {
+         smallElectricDetonation = new Effect(30f, (e) -> {
+            Draw.color(Color.valueOf("97B5EDFF"), e.color, e.fin() + 0.4F);
+            e.scaled(6, (i) -> {
+                Lines.stroke(3f * i.foutpow());
+                Lines.circle(i.x, i.y, i.fin(Interp.circleOut) * 15f * 15F);
+            });
+            Angles.randLenVectors((long) e.id, 6, 21f * e.finpow(), (x, y) -> {
+                Fill.circle(e.x, e.y, 15f * e.fout() * 4f);
+            });
+            Angles.randLenVectors((long) e.id, 6, 20f * e.finpow(), (x, y) -> {
+                float len = 15f * e.fout() * Mathf.random(4f, 8f) + 2f;
+                Lines.lineAngle(e.x + x, e.y + y, 15f * e.fout(), len);
+            });
         });
-        Angles.randLenVectors((long)e.id, 6, 21f * e.finpow(), (x, y) -> {
-            Fill.circle(e.x, e.y, 15f * e.fout() * 4f);
-        });
-        Angles.randLenVectors((long)e.id, 6, 20f * e.finpow(), (x, y) -> {
-            float len = 15f * e.fout() * Mathf.random(4f, 8f) + 2f;
-            Lines.lineAngle(e.x + x, e.y + y, 15f * e.fout(), len);
-        });
-    });
-
-    public static void load(){}
+    }
 }
