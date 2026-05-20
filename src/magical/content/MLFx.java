be618f19c2e1c048023508c94558c08166b2a664
magical <3030854060@qq.com>
2026-05-20 16:59:33 +0800

    magic

--- src/magical/content/MLFx.java
+++ src/magical/content/MLFx.java
@@ -14,26 +14,26 @@ import arc.math.geom.Vec2;
 import mindustry.entities.Effect;
 
 public class MLFx {
-    public static final Rand rand = new Rand();
+    public static final Rand rand = new rand();
 
-    public static Effect smallElectricDetonation = new Effect(22, e -> {
-        color(Color.valueOf("97B5EDFF"));
+    public static Effect smallElectricDetonation = new Effect(22f, e -> {
+        Draw.color(Color.valueOf("97B5EDFF"));
 
         e.scaled(6, i -> {
-            stroke(3f * i.fout());
+            Lines.stroke(3f * i.fout());
             Lines.circle(e.x, e.y, 3f + i.fin() * 15f);
         });
 
-        color(Color.valueOf("97B5EDFF"));
+        Draw.color(Color.valueOf("97B5EDFF"));
 
-        randLenVectors(e.id, 6, 2f + 20f * e.finpow(), (x, y) -> {
+        rand(e.id, 6, 2f + 20f * e.finpow(), (x, y) -> {
             Fill.circle(e.x + x, e.y + y, e.fout() * 3f + 0.5f);
         });
 
-        color(Color.valueOf("97B5EDFF"));
+        Draw.color(Color.valueOf("97B5EDFF"));
 
-        randLenVectors(e.id + 1, 4, 1f + 20f * e.finpow(), (x, y) -> {
-            lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), 1f + e.fout() * 3f);
+        rand(e.id + 1, 4, 1f + 20f * e.finpow(), (x, y) -> {
+            lineAngle(e.x + x, e.y + y, mathf.angle(x, y), 1f + e.fout() * 3f);
         });
 
         Drawf.light(e.x, e.y, 45f, Color.valueOf("97B5EDFF"), 0.8f * e.fout());
