7f6b3e31539cb302da9a0c39238b38ea61914008
magical <3030854060@qq.com>
2026-05-30 00:50:02 +0800

    magic

--- src/magical/content/MLBlocks.java
+++ src/magical/content/MLBlocks.java
@@ -260,7 +260,7 @@ public class MLBlocks {
             colors = new Color[]{Color.valueOf("596ab8").a(0.55f), Color.valueOf("596ab8").a(0.7f), Color.valueOf("596ab8").a(0.8f), Color.valueOf("596ab8"), Color.white};
             flareColor = Color.valueOf("596ab8");
                }};
+            );
         }};
-        );
     }
 }
