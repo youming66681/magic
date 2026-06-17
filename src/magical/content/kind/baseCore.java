533062ce68d7e43a5cf7a8a207589a29dd4ce175
magical <3030854060@qq.com>
2026-06-17 18:31:52 +0800

    magic

--- src/magical/content/kind/baseCore.java
+++ src/magical/content/kind/baseCore.java
@@ -7,49 +7,38 @@ import mindustry.world.blocks.storage.CoreBlock;
 import mindustry.world.Tile;
 import mindustry.game.Team;
 import mindustry.Vars;
-import arc.Core;
-import mindustry.graphics.Pal;
-import mindustry.ui.Bar;
 
-public class baseCore extends CoreBlock {
+public class baseCore extends CoreBlock{
 
     public baseCore(String name) {
         super(name);
     }
 
-    public boolean canPlace(Tile tile, Team team, int rotation){
-        if(tile == null) return false;
-
-        if(Vars.state.isEditor()) return true;
-
-        return Vars.state.teams.cores(team).size < 10;
-    }
-
     @Override
-    public boolean canBreak(Tile tile) {
+    public boolean canBreak(Tile tile){
         return Vars.state.teams.cores(tile.team()).size > 1;
     }
 
     @Override
-    public boolean canPlaceOn(Tile tile, Team team, int rotation) {
-        if (tile == null) return false;
+    public boolean canPlaceOn(Tile tile, Team team, int rotation){
+        if(tile == null) return false;
         //in the editor, you can place them anywhere for convenience
-        if (Vars.state.isEditor()) return true;
+        if(Vars.state.isEditor()) return true;
+        if(!Vars.state.isEditor()) return true;
 
         CoreBuild core = team.core();
 
-        if(Vars.state.teams.cores(team).size >= 10)
-            return false;
-
         //special floor upon which cores can be placed
+        tile.getLinkedTilesAs(this, tempTiles);
+        if(!tempTiles.contains(o -> !o.floor().allowCorePlacement || o.block() instanceof CoreBlock)){
+            return true;
+        }
 
         //must have all requirements
-        if (core == null || (!Vars.state.rules.infiniteResources && !core.items.has(requirements, Vars.state.rules.buildCostMultiplier)))
-            return false;
+        if(core == null || (!Vars.state.rules.infiniteResources && !core.items.has(requirements, Vars.state.rules.buildCostMultiplier))) return false;
 
         return tile.block() instanceof CoreBlock && size > tile.block().size && (!requiresCoreZone || tempTiles.allMatch(o -> o.floor().allowCorePlacement));
     }
-
     @Override
     public void drawPlace(int x, int y, int rotation, boolean valid){
         super.drawPlace(x, y, rotation, valid);
