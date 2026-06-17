c6ca3b63713f18bbccb1bf97328e352a635b9c59
magical <3030854060@qq.com>
2026-06-17 17:42:00 +0800

    magic

--- src/magical/content/kind/baseCore.java
+++ src/magical/content/kind/baseCore.java
@@ -11,43 +11,45 @@ import arc.Core;
 import mindustry.graphics.Pal;
 import mindustry.ui.Bar;
 
-public class baseCore extends CoreBlock{
+public class baseCore extends CoreBlock {
 
     public baseCore(String name) {
         super(name);
     }
 
     @Override
-    public boolean canBreak(Tile tile){
+    public boolean canBreak(Tile tile) {
         return Vars.state.teams.cores(tile.team()).size > 1;
     }
 
     @Override
-    public boolean canPlaceOn(Tile tile, Team team, int rotation){
-        if(tile == null) return false;
+    public boolean canPlaceOn(Tile tile, Team team, int rotation) {
+        if (tile == null) return false;
         //in the editor, you can place them anywhere for convenience
-        if(Vars.state.isEditor()) return true;
-        if(!Vars.state.isEditor()) return true;
+        if (Vars.state.isEditor()) return true;
+        if (!Vars.state.isEditor()) return true;
 
         CoreBuild core = team.core();
 
-        if(Vars.state.teams.cores(team).size >= 10){
+        if (Vars.state.teams.cores(team).size >= 10) {
             return false;
         }
 
         //special floor upon which cores can be placed
         tile.getLinkedTilesAs(this, tempTiles);
-        if(!tempTiles.contains(o -> !o.floor().allowCorePlacement || o.block() instanceof CoreBlock)){
+        if (!tempTiles.contains(o -> !o.floor().allowCorePlacement || o.block() instanceof CoreBlock)) {
             return true;
         }
 
         //must have all requirements
-        if(core == null || (!Vars.state.rules.infiniteResources && !core.items.has(requirements, Vars.state.rules.buildCostMultiplier))) return false;
+        if (core == null || (!Vars.state.rules.infiniteResources && !core.items.has(requirements, Vars.state.rules.buildCostMultiplier)))
+            return false;
 
         return tile.block() instanceof CoreBlock && size > tile.block().size && (!requiresCoreZone || tempTiles.allMatch(o -> o.floor().allowCorePlacement));
     }
+
     @Override
-    public void setBars(){
+    public void setBars() {
         super.setBars();
 
         addBar("core-count", build ->
@@ -58,7 +60,8 @@ public class baseCore extends CoreBlock{
                                 10
                         ),
                         () -> Pal.accent,
-                        () -> (float)Vars.state.teams.cores(build.team).size / 10f
+                        () -> (float) Vars.state.teams.cores(build.team).size / 10f
                 )
         );
+    }
 }
