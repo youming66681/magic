777a68ff13092a080f1bb2762c7431850cabf2fb
magical <3030854060@qq.com>
2026-06-23 15:47:44 +0800

    magic

--- src/magical/content/kind/UpgradePath.java
+++ src/magical/content/kind/UpgradePath.java
@@ -8,6 +8,11 @@ public class UpgradePath{
     public UnitType from;
     public UnitType to;
 
+    public consumePower();
+    public consumeItems(ItemStack.with(new Object[]{MLItems.phantomSteel, 60, Items.graphite, 30}));
+
+    public constructTime = 60f * 15f;
+
     public UpgradePath(String bundleKey, UnitType from, UnitType to){
         this.bundleKey = bundleKey;
         this.from = from;
