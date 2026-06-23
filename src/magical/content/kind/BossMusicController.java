b52f8d6e36fe4ae5c30b9ecde6f8ea87d26e5b2e
magical <3030854060@qq.com>
2026-06-23 17:25:52 +0800

    magic

--- src/magical/content/kind/BossMusicController.java
+++ src/magical/content/kind/BossMusicController.java
@@ -1,13 +1,18 @@
 package magical.content;
 
 import arc.Events;
+import arc.math.Mathf;
+import arc.util.Time;
+import mindustry.Vars;
 import mindustry.game.EventType;
 import mindustry.gen.Groups;
 import mindustry.gen.Unit;
+import mindustry.type.UnitType;
 
 public class BossMusicController {
 
     private static boolean playing = false;
+    private static float fade = 1f;
 
     public static void init(){
 
@@ -15,9 +20,9 @@ public class BossMusicController {
 
             boolean bossAlive = false;
 
-            for(Unit unit : Groups.unit){
+            for(Unit u : Groups.unit){
 
-                if(unit.isBoss()){
+                if(isBoss(u)){
                     bossAlive = true;
                     break;
                 }
@@ -26,24 +31,46 @@ public class BossMusicController {
             if(bossAlive && !playing){
 
                 playing = true;
+                fade = 1f;
 
                 if(MLMusic.boss1 != null){
-
                     MLMusic.boss1.setLooping(true);
-
+                    MLMusic.boss1.setVolume(0f);
                     MLMusic.boss1.play();
                 }
             }
 
-            if(!bossAlive && playing){
+            if(playing && MLMusic.boss1 != null){
 
-                playing = false;
+                if(bossAlive){
 
-                if(MLMusic.boss1 != null){
+                    fade = Mathf.lerp(fade, 1f, 0.05f);
+                }else{
+
+                    fade = Mathf.lerp(fade, 0f, 0.05f);
+                }
+
+                MLMusic.boss1.setVolume(fade);
+
+                if(!bossAlive && fade < 0.01f){
 
                     MLMusic.boss1.stop();
+                    playing = false;
                 }
             }
         });
+    }
+
+    private static boolean isBoss(Unit u){
+
+        if(u == null) return false;
+
+        UnitType t = u.type;
+
+        if(t == null) return false;
+
+        // 1. 官方 Wave Boss
+        if (u.isBoss()) return true;
+
     }
 }
