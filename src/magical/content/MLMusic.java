bef2af6c7561e0acadc81e27c2b0d784a223ce7a
magical <3030854060@qq.com>
2026-06-23 16:50:43 +0800

    magic

--- src/magical/content/MLMusic.java
+++ src/magical/content/MLMusic.java
@@ -1,15 +1,29 @@
 package magical.content;
 
+import magical.magic;
+import arc.audio.Sound;
+import mindustry.Vars;
+import mindustry.mod.Mods;
 import arc.Core;
 import arc.audio.Music;
 
-public class MLMusic{
-
+public class MLMusic {
+    public static Mods.LoadedMod ML;
     public static Music boss1;
     public static Music boss2;
 
-    public static void load(){
-        boss1 = Core.assets.load("music/boss1.ogg", Music.class).loaded;
-        boss2 = Core.assets.load("music/boss1.ogg", Music.class).loaded;
+    public static void load() {
+        boss1 = loadMusic("boss1.ogg");
+        boss2 = loadMusic("boss1.ogg");
+    }
+
+    private static Music loadMusic(String name) {
+        return new Sound(ML.root.child("music").child(name));
+    }
+
+    static {
+        ML = Vars.mods.getMod(magic.class);
+        boss1 = new Music();
+        boss2 = new Music();
     }
 }
