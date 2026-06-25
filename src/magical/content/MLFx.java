b04a2dbe5f5f1429e25ed950cdee2d8c9632793b
magical <3030854060@qq.com>
2026-06-25 17:20:36 +0800

    magic

--- src/magical/content/MLFx.java
+++ src/magical/content/MLFx.java
@@ -24,7 +24,7 @@ public class MLFx {
     public static final Rand rand = new Rand();
     Vec2 temp = new Vec2();
 
-    public static tri(float x, float y, float width, float length, float angle) {
+    public static void tri(float x, float y, float width, float length, float angle) {
         float wx = Angles.trnsx(angle + 90, width), wy = Angles.trnsy(angle + 90, width);
         Fill.tri(x + wx, y + wy, x - wx, y - wy, Angles.trnsx(angle, length) + x, Angles.trnsy(angle, length) + y);
     }
