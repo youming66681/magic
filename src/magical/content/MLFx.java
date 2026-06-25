c2cd33576c59a0c749ed03189a8d8ef5bf1150bd
magical <3030854060@qq.com>
2026-06-25 18:26:44 +0800

    magic

--- src/magical/content/MLFx.java
+++ src/magical/content/MLFx.java
@@ -58,23 +58,13 @@ public class MLFx {
     public static Effect Slash(Color colorExternal, Color colorInternal, float len, float width){
         return new Effect(30f, e -> {
 
-            Draw.color(colorExternal);
+            Draw.color(colorInternal);
             //外层光
             Drawf.tri(e.x, e.y, width * e.fout(), len, e.rotation);
             Drawf.tri(e.x, e.y, width * e.fout(), len, e.rotation + 180f);
-            //内层高
-            //Drawf.tri(e.x, e.y, width * 0.5f, len * 0.8f * e.fout(), e.rotation
-            //);
-            //裂纹
-            /*Draw.color(colorInternal);
-            Angles.randLenVectors(e.id, 15, len * e.fin(), e.rotation, 30f, (x, y) -> {
-                        Lines.stroke(1.5f * e.fout());
-                        Lines.lineAngle(e.x + x, e.y + y, Mathf.angle(x, y), 8f * e.fout());
-                    }
-            );*/
-            //冲击波
-            //Lines.stroke(3f * e.fout());
-            //Lines.circle(e.x, e.y, 50f * e.fin());
+            Draw.color(colorExternal, 0.5f); // 更透明一点
+            Drawf.tri(e.x, e.y, width * 0.5 * e.fout(), len, e.rotation);
+            Drawf.tri(e.x, e.y, width * 0.5 * e.fout(), len, e.rotation + 180f);
         });
     }
 }
