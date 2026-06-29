24540fa0c43e30199d81c6d7627512bd5d1fd7c4
magical <3030854060@qq.com>
2026-06-30 01:33:37 +0800

    magic

--- assets/scripts/blocks/items.js
+++
@@ -1,9 +0,0 @@
-function newItem(name) {
-	exports[name] = (() => {
-		let myItem = extend(Item, name, {});
-		return myItem;
-	})();
-}
-newItem("phantomSteel")
-newItem("phantomTitaniumSteel")
-newItem("mysticCrystal")
--- assets/scripts/main.js
+++
@@ -1,6 +0,0 @@
-MapResizeDialog.minSize = 0
-MapResizeDialog.maxSize = 50000
-require("blocks/chipMachine");
-require("base/lib");
-require("base/library");
-require("items");
