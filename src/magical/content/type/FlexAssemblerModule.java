84222dd3a75c2eed661072b50e8d6936ede3232d
magical <3030854060@qq.com>
2026-07-22 22:34:21 +0800

    magic

--- src/magical/content/type/FlexAssemblerModule.java
+++ src/magical/content/type/FlexAssemblerModule.java
@@ -1,19 +1,28 @@
 package magical.content;
+
+import arc.util.*;
+import mindustry.game.Team;
 import mindustry.gen.*;
 import mindustry.world.*;
 import mindustry.world.blocks.units.*;
+
+import static mindustry.Vars.*;
+
 public class FlexAssemblerModule extends UnitAssemblerModule {
+
     public FlexAssemblerModule(String name) {
         super(name);
     }
+
     @Override
     public boolean canPlaceOn(Tile tile, Team team, int rotation) {
         if (!super.canPlaceOn(tile, team, rotation)) return false;
+
         // 查找相邻的 FlexAssembler
         for (int dx = -1; dx <= 1; dx++) {
             for (int dy = -1; dy <= 1; dy++) {
                 if (dx == 0 && dy == 0) continue;
-                Tile other = world.tile(tile.x + dx, tile.y + dy);
+                Tile other = Vars.world.tile(tile.x + dx, tile.y + dy);
                 if (other != null && other.build instanceof FlexAssembler.FlexAssemblerBuild assembler) {
                     if (assembler.moduleFits(this, tile.worldx(), tile.worldy(), rotation)) {
                         return true;
