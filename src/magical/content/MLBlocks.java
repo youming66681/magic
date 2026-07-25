de901d91e950e482a45de55d06f1627428f1bfb5
magical <3030854060@qq.com>
2026-07-25 14:02:11 +0800

    magic

--- src/magical/content/MLBlocks.java
+++ src/magical/content/MLBlocks.java
@@ -925,8 +925,8 @@ public class MLBlocks {
                             y = 0f;
                             moveX = 8f;
                             moveY = 0f;
-                        }},
-            }},
+                        }}
+            }};
             shootType = new BasicBulletType(16f, 480f){{
                 hitEffect = MLFx.colorSparkBig;
                 despawnEffect = hitEffect;
