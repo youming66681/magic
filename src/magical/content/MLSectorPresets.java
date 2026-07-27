b17cb76daa2a5412a95f9e6adde26a976a6a7d14
magical <3030854060@qq.com>
2026-07-27 22:53:57 +0800

    magic

--- src/magical/content/MLSectorPresets.java
+++ src/magical/content/MLSectorPresets.java
@@ -5,13 +5,13 @@ import mindustry.type.*;
 
 import static mindustry.content.Planets.*;
 
-public class SectorPresets{
+public class MLSectorPresets{
     public static SectorPreset
             LandingZone;
 
     public static void load(){
 
-        LandingZone = new SectorPreset("LandingZone", cecilia, 0){{
+        LandingZone = new SectorPreset("LandingZone", MLPlanets.cecilia, 0){{
             alwaysUnlocked = true;
             addStartingItems = true;
             captureWave = 10;
