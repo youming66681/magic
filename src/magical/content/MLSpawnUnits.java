ee6a438a7ce2edf9dc81f5004dcf3e3c107a7c1e
magical <3030854060@qq.com>
2026-07-27 14:05:32 +0800

    magic

--- src/magical/content/MLSpawnUnits.java
+++ src/magical/content/MLSpawnUnits.java
@@ -73,7 +73,7 @@ public class MLSpawnUnits {
                                 haloRadius = 10f;
                                 haloRadiusTo = 10f;
                                 haloRotateSpeed = 3f;
-                            }}
+                            }},
                         new HaloPart(){{
                                 sides = 3;
                                 shapes = 3;
@@ -88,7 +88,7 @@ public class MLSpawnUnits {
                                 haloRadius = 15f;
                                 haloRadiusTo = 15f;
                                 haloRotateSpeed = -3f;
-                            }}
+                            }};
                           );
                         despawnUnit = MLUnitTypes.Starlight;
                         despawnUnitRadius = 0f;
