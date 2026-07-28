4040a0fd1dba2e96c4180d78e9ba7e01ff50e6c3
magical <3030854060@qq.com>
2026-07-28 22:24:31 +0800

    magic

--- src/magical/content/MLSpawnUnits.java
+++ src/magical/content/MLSpawnUnits.java
@@ -34,7 +34,7 @@ public class MLSpawnUnits {
                 // 立即移除原始单位，避免它短暂出现
                 unit.remove();
                 // 延迟后重新创建单位
-                Effect.shake(5f, 0.5f, 0, 0).at(x, y);//强度，时间
+                Effect.shake(5f, 0.5f, 0, 0);//强度，时间
                 Time.run(delay * 35f, () -> {
                     Unit newUnit = type.create(state.rules.waveTeam);
                     newUnit.set(x, y);
@@ -48,7 +48,7 @@ public class MLSpawnUnits {
                     UnitType type = unit.type;
                     MLFx.smallTeleport.at(x, y);
                     unit.remove();
-                    Effect.shake(5f, 0.5f, 0, 0).at(x, y);
+                    Effect.shake(5f, 0.5f, 0, 0);
                     Time.run(delay * 35f, () -> {
                         Unit newUnit = type.create(state.rules.waveTeam);
                         newUnit.set(x, y);
