ec7a00ebe6a413b0a19602c8c9d93cbae09abacc
magical <3030854060@qq.com>
2026-08-06 12:45:36 +0800

    magic

--- src/magical/content/MLBlocks.java
+++ src/magical/content/MLBlocks.java
@@ -2288,7 +2288,7 @@ public class MLBlocks {
             hardnessDrillMultiplier = 0.9f;
             squareSprite = false;
             size = 5;
-            drawMineItem = false
+            drawMineItem = false;
             drawRim = true;
             tier = 10;
             updateEffect = MLFx.energyMine;
