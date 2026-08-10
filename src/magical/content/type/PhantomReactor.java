ffeedc330c34a6975af232df9355c45801d85c08
magical <3030854060@qq.com>
2026-08-10 15:41:46 +0800

    magic

--- src/magical/content/type/PhantomReactor.java
+++ src/magical/content/type/PhantomReactor.java
@@ -40,8 +40,8 @@ public class PhantomReactor extends PowerGenerator {
     public Liquid fuelLiquid = MLLiquids.PhantomSteelSolution;
     public Liquid coolantLiquid = Liquids.water;
 
-    public float fuelLiquidAmount = fuelLiquid;
-    public float coolantAmount = coolantLiquid;
+    public float fuelLiquidAmount = 1f;
+    public float coolantAmount = 1f;
 
     public TextureRegion topRegion;
     public TextureRegion lightsRegion;
