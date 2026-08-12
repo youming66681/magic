a91660df59d59002894c67be01818ab73f20abc9
magical <3030854060@qq.com>
2026-08-12 21:45:32 +0800

    magic

--- src/magical/content/MLBlocks.java
+++ src/magical/content/MLBlocks.java
@@ -2642,7 +2642,7 @@ public class MLBlocks {
                     new PayloadStack(MLUnitTypes.Drizzle, 3));
             addPlan("T1", MLUnitTypes.Storm, 1800f, 9, 0,
                     new PayloadStack(MLBlocks.largePhantomSteelWall, 6),
-                    new PayloadStack(MLUnitTypes.Breeze, 3),
+                    new PayloadStack(MLUnitTypes.Breeze, 3);
 
             consumePower(20f);
         }};
