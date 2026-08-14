feda01c39cf0b3d404ca26b6bc86d9ed20192fb7
magical <3030854060@qq.com>
2026-08-14 20:30:15 +0800

    magic

--- src/magical/content/MLUnitTypes.java
+++ src/magical/content/MLUnitTypes.java
@@ -1537,7 +1537,7 @@ public class MLUnitTypes {
                                 backColor = Color.valueOf("958F60FF");
                                 splashDamageRadius = 48;
                                 splashDamage = 350;
-                                incendAmount = 2.5f;
+                                incendAmount = 5;
                                 incendSpread = 5;
                                 incendChance = 0.25f;
                                 spin = 2.5f;
