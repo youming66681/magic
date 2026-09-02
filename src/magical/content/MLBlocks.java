c8c9cbe95f0b3d07337a7950a4c244a58d47b97b
magical <3030854060@qq.com>
2026-09-02 21:42:53 +0800

    magic

--- src/magical/content/MLBlocks.java
+++ src/magical/content/MLBlocks.java
@@ -605,7 +605,7 @@ public class MLBlocks {
             drawer = new DrawMulti(new DrawRegion("-bottom"), new DrawDefault(), new DrawFlame());
 
             consumePower(20f);
-            consumeItems(ItemStack.with(Items.phantomLuminousAlloy, 1, MLItems.nanoEnergyThread, 1, MLItems.crystallineCarbon, 1));
+            consumeItems(ItemStack.with(MLItems.phantomLuminousAlloy, 1, MLItems.nanoEnergyThread, 1, MLItems.crystallineCarbon, 1));
         }};
         //factor
         //炮
