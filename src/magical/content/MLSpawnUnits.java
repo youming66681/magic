fe2ecf1b5eb8eb305d545ed6b1bd84ceec4ec150
magical <3030854060@qq.com>
2026-09-09 19:12:22 +0800

    magic

--- src/magical/content/MLSpawnUnits.java
+++ src/magical/content/MLSpawnUnits.java
@@ -38,8 +38,8 @@ public class MLSpawnUnits {
                     Unit newUnit = type.create(state.rules.waveTeam);
                     newUnit.set(x, y);
                     MLSounds.shootRipple.at(x, y, 5f);
-                    MLFx.jumpTrail.at(unit.x, unit.y, unit.rotation, unit.team.color, unit.type);
                     newUnit.add();
+                    MLFx.jumpTrail.at(x, y, unit.rotation, unit.team.color, unit.type);
                 });
             }
                 else if (unit.type == MLUnitTypes.Qingxiao) {
@@ -53,8 +53,8 @@ public class MLSpawnUnits {
                         Unit newUnit = type.create(state.rules.waveTeam);
                         newUnit.set(x, y);
                         MLSounds.shootRipple.at(x, y, 5f);
-                        MLFx.jumpTrail.at(unit.x, unit.y, unit.rotation, unit.team.color, unit.type);
                         newUnit.add();
+                        MLFx.jumpTrail.at(x, y, unit.rotation, unit.team.color, unit.type);
                     });
                 }
             else if (unit.type == MLUnitTypes.BrokenEdge) {
