a4f903925bf9c72b2cf968379737cdf986bf8fa1
magical <3030854060@qq.com>
2026-09-10 18:18:41 +0800

    magic

--- src/magical/content/abilities/DamageReductionAbility.java
+++ src/magical/content/abilities/DamageReductionAbility.java
@@ -1,13 +1,15 @@
-package magical.content;
+package magical.content.abilities;
 import arc.scene.ui.layout.Table;
 import arc.util.Strings;
 import mindustry.entities.abilities.Ability;
 import mindustry.gen.Unit;
 public class DamageReductionAbility extends Ability{
+    /** 减伤比例，0.3f = 30% */
     public float reduction;
     public DamageReductionAbility(float reduction){
         this.reduction = Math.max(0f, Math.min(reduction, 0.9999f));
     }
+    @Override
     public float modifyDamage(Unit unit, float amount){
         return amount * (1f - reduction);
     }
