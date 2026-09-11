f5192ffb1a47f708114d1593d7cdc87aaa43ca5d
magical <3030854060@qq.com>
2026-09-11 20:00:10 +0800

    magic

--- src/magical/content/abilities/TargetTeleportAbility.java
+++ src/magical/content/abilities/TargetTeleportAbility.java
@@ -6,35 +6,32 @@ import mindustry.entities.Effect;
 import mindustry.entities.abilities.Ability;
 import mindustry.gen.Groups;
 import mindustry.gen.Unit;
-import mindustry.ai.AIController;
 import mindustry.gen.Teamc;
 import mindustry.gen.Unit;
+import mindustry.type.Weapon;
 
 public class TargetTeleportAbility extends Ability{
     public Effect teleportEffect;
-
     public TargetTeleportAbility(Effect teleportEffect){
         this.teleportEffect = teleportEffect;
     }
-
     @Override
     public void update(Unit unit){
-        if(!(unit.controller() instanceof AIController ai))return;
-
-        Teamc target = ai.target;
-
-        if(!(target instanceof Unit enemy))return;
-        if(!enemy.isValid())return;
-        if(enemy.team == unit.team)return;
-
+        Unit target = null;
+        for(Weapon weapon : unit.type.weapons){
+            if(weapon.bullet == null)continue;
+            if(unit.isShooting()){
+                target = unit.aimTarget instanceof Unit ? (Unit)unit.aimTarget : null;
+                if(target != null)break;
+            }
+        }
+        if(target == null)return;
+        if(!target.isValid())return;
+        if(target.team == unit.team)return;
         float selfRange = unit.type.range;
-
-        if(unit.within(enemy.x, enemy.y, selfRange))return;
-
-        if(enemy.type.range <= selfRange)return;
-
-        unit.set(enemy.x, enemy.y);
-
+        if(unit.within(target.x,target.y,selfRange))return;
+        if(target.type.range <= selfRange)return;
+        unit.set(target.x,target.y);
             if(teleportEffect != null){
                 teleportEffect.at(
                         unit.x,
