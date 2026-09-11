9d47056945788ab7bd97d4e78341c609c8a38a36
magical <3030854060@qq.com>
2026-09-11 22:39:32 +0800

    magic

--- src/magical/content/kind/UnitFocusEvent.java
+++ src/magical/content/kind/UnitFocusEvent.java
@@ -5,12 +5,10 @@ public class UnitFocusEvent{
     public float x;
     public float y;
     public float time;
-    public String message;
 
-    public UnitFocusEvent(float x,float y,float time,String message){
+    public UnitFocusEvent(float x,float y,float time){
         this.x = x;
         this.y = y;
         this.time = time;
-        this.message = message;
     }
 }
