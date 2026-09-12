6b0fbd9294d0a59f5f6f4c534bcb685252d865a4
magical <3030854060@qq.com>
2026-09-12 21:59:49 +0800

    magic

--- src/magical/content/MLPlanets.java
+++ src/magical/content/MLPlanets.java
@@ -67,18 +67,14 @@ public class MLPlanets {
             landCloudColor = Pal.spore.cpy().a(0.5f);
              ruleSetter = rules -> {
                  rules.hideBannedBlocks = true;
-
                  for(Block block : Vars.content.blocks()){
                      Class<?> type = block.getClass();
-
                      while(type != null){
                          String name = type.getSimpleName();
-
                          if(name.equals("LaunchPad") || name.equals("LandingPad")){
                              rules.bannedBlocks.add(block);
                              break;
                          }
-
                          type = type.getSuperclass();
                      }
                  }
